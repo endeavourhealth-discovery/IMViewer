a45309106e71befc328edb52c89f1cc655258955
matt-voror <matt@voror.co.uk>
2022-04-01 13:45:35 +0100

    swapped to use imLibrary type methods over isTypeOf

--- src/views/Concept.vue
+++ src/views/Concept.vue
@@ -132,7 +132,7 @@ import { Helpers, Vocabulary, LoggerService } from "im-library";
 import { DefinitionConfig, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
 const { IM, RDF, RDFS, SHACL } = Vocabulary;
 const {
-  ConceptTypeMethods: { isOfTypes, isProperty, isValueSet },
+  ConceptTypeMethods: { isOfTypes, isProperty, isValueSet, isConcept, isQuery, isFolder, isRecordModel },
   CopyConceptToClipboard: { copyConceptToClipboard, conceptObjectToCopyString },
   DataTypeCheckers: { isObjectHasKeys },
   Sorters: { byOrder }
@@ -163,23 +163,23 @@ export default defineComponent({
     },
 
     showMappings(): boolean {
-      return (isOfTypes(this.types, IM.CONCEPT) || isOfTypes(this.types, RDFS.CLASS)) && !isOfTypes(this.types, SHACL.NODESHAPE);
+      return (isConcept(this.types) || isOfTypes(this.types, RDFS.CLASS)) && !isRecordModel(this.types);
     },
 
     isConcept(): boolean {
-      return isOfTypes(this.types, IM.CONCEPT);
+      return isConcept(this.types);
     },
 
     isQuery(): boolean {
-      return isOfTypes(this.types, IM.QUERY_TEMPLATE);
+      return isQuery(this.types);
     },
 
     isRecordModel(): boolean {
-      return isOfTypes(this.types, SHACL.NODESHAPE);
+      return isRecordModel(this.types);
     },
 
     isFolder(): boolean {
-      return isOfTypes(this.types, IM.FOLDER);
+      return isFolder(this.types);
     },
 
     isProperty(): boolean {
