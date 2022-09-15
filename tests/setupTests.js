015e9cee73064c87e8fced4d8d69cda8265c0147
matt-voror <matt@voror.co.uk>
2022-09-15 14:19:58 +0100

    added msw server to setupTests to cover blocking all fetch requests during testing

---
+++ tests/setupTests.js
@@ -0,0 +1,16 @@
+import { setupServer } from "msw/node";
+
+const restHandlers = [];
+const server = setupServer(...restHandlers);
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterAll(() => {
+  server.close();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
